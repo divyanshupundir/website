c2f1f740a4725bc2985fd9875194eb009d875693
divyanshupundir <divyanshu.pundir@gmail.com>
2024-05-17 20:39:49 +0530

    feat: Add items

--- src/app/intro-section.tsx
+++ src/app/intro-section.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 export function IntroSection() {
@@ -13,7 +14,26 @@ export function IntroSection() {
       <div className="absolute h-full w-full bg-gradient-to-r from-black/90 to-black/70" />
       <div className="absolute">
         <h1 className="m-8 font-mono text-6xl">divyanshu pundir</h1>
+        <div className="m-8 flex gap-8">
+          <Item first="software" second="engineer" />
+          <Item first="android" second="developer" />
+          <Item first="open-source" second="contributor" />
+        </div>
+      </div>
+      <div className="absolute bottom-0 m-8 flex w-screen gap-8">
+        <Item first="read" second="blog" />
+        <Item first="follow" second="github" />
+        <Item first="connect" second="lnkedin" />
+        <Item first="view" second="behance" />
       </div>
     </section>
+  );
+}
+
+function Item({ first, second }: { first: string; second: string }) {
+  return (
+    <p className="text-sm">
+      {first} <span className="text-2xl font-bold">{second}</span>
+    </p>
   );
 }
