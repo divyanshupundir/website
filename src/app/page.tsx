4274f8437fb64c8aca9a173d3401f5cb4cdecc61
divyanshupundir <divyanshu.pundir@gmail.com>
2024-05-18 15:59:21 +0530

    refactor: Remove timeline section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { IntroSection } from "./intro-section";
-import { TimelineSection } from "./timeline-section";
 
 export default function HomePage() {
   return (
     <main>
       <IntroSection />
-      <TimelineSection />
     </main>
   );
 }
--- src/app/timeline-section.tsx
+++
@@ -1,3 +0,0 @@
-export function TimelineSection() {
-  return <section className="h-screen w-screen">timeline section</section>;
-}
