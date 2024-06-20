34f931cdb64845316e4e298d296c19001d576e02
divyanshupundir <divyanshu.pundir@gmail.com>
2024-06-20 16:18:04 +0530

    refactor: Better handling of not found post

--- src/app/blog/[postId]/page.tsx
+++ src/app/blog/[postId]/page.tsx
@@ -1,17 +1,32 @@
 import { notFound } from "next/navigation";
+import { getAllPosts } from "../post-data";
+import Link from "next/link";
 
 export default async function PostPage({
   params,
 }: {
   params: { postId: string };
 }) {
-  let PostContent;
+  const data = getAllPosts().find((post) => post.id === params.postId);
 
-  try {
-    PostContent = (await import(`../content/${params.postId}.mdx`)).default;
-  } catch (error) {
+  if (!data) {
     notFound();
   }
+
+  if (data.externalLink) {
+    return (
+      <div className="flex h-screen flex-col items-center justify-center">
+        <div className="text-2xl">this post is hosted externally</div>
+        <Link href={data.externalLink}>
+          <div className="space-y-2 text-2xl transition-colors duration-500 hover:text-primary">
+            take me there
+          </div>
+        </Link>
+      </div>
+    );
+  }
+
+  const PostContent = (await import(`../content/${params.postId}.mdx`)).default;
 
   return (
     <div className={"prose m-4 sm:m-8 sm:mx-auto"}>
