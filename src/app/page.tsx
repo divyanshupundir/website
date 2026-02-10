33478ce6bf043bc900045a56404b67f8d4ca29a2
divyanshupundir <divyanshu.pundir@gmail.com>
2026-02-10 23:45:38 +0530

    refactor: Update typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function HomePage() {
-  return (
-    <main className="h-screen w-screen">
-      <Image
-        src="/home.jpg"
-        alt="Home"
-        fill
-        className="h-full w-full object-cover"
-      />
-      <div className="absolute h-full w-full bg-gradient-to-r from-background/90 to-background/70" />
-      <div className="absolute">
-        <h1 className="m-4 text-4xl sm:m-8 sm:text-6xl">divyanshu pundir</h1>
-        <div className="m-4 flex flex-col gap-4 sm:m-8 sm:flex-row sm:gap-8">
-          <Item first="software" second="engineer" />
-          <Item first="product" second="developer" />
-          <Item first="startup" second="founder" />
-        </div>
-      </div>
-      <div className="absolute bottom-0 m-4 flex flex-col gap-4 sm:m-8 sm:flex-row sm:gap-8">
-        <ItemWithLink first="read" second="blog" link="/blog" />
-        <ItemWithLink
-          first="follow"
-          second="github"
-          link="https://github.com/divyanshupundir"
-        />
-        <ItemWithLink
-          first="connect"
-          second="linkedin"
-          link="https://www.linkedin.com/in/divyanshu-pundir-212933145/"
-        />
-        <ItemWithLink
-          first="view"
-          second="behance"
-          link="https://www.behance.net/divyanshupundir"
-        />
-      </div>
-    </main>
-  );
-}
-
 function Item({ first, second }: { first: string; second: string }) {
   return (
     <p className="text-xs sm:text-sm">
@@ -66,3 +26,44 @@ function ItemWithLink({
     </Link>
   );
 }
+
+export default function HomePage() {
+  return (
+    <main className="h-screen w-screen">
+      <Image
+        src="/home.jpg"
+        alt="Home"
+        fill
+        className="h-full w-full object-cover"
+      />
+      <div className="absolute h-full w-full bg-gradient-to-r from-background/90 to-background/70" />
+      <div className="absolute">
+        <h1 className="m-4 text-4xl sm:m-8 sm:text-6xl">Divyanshu Pundir</h1>
+        <div className="m-4 flex flex-col gap-4 sm:m-8 sm:flex-row sm:gap-8">
+          <Item first="Software" second="Engineer" />
+          <Item first="Product" second="Developer" />
+          <Item first="Startup" second="Founder" />
+        </div>
+      </div>
+      <div className="absolute bottom-0 m-4 flex flex-col gap-4 sm:m-8 sm:flex-row sm:gap-8">
+        <ItemWithLink first="Read" second="Blog" link="/blog" />
+        <ItemWithLink
+          first="Follow"
+          second="GitHub"
+          link="https://github.com/divyanshupundir"
+        />
+        <ItemWithLink
+          first="Connect"
+          second="LinkedIn"
+          link="https://www.linkedin.com/in/divyanshu-pundir-212933145/"
+        />
+        <ItemWithLink
+          first="View"
+          second="Behance"
+          link="https://www.behance.net/divyanshupundir"
+        />
+      </div>
+    </main>
+  );
+}
+
